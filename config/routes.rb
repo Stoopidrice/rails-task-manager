607d370fd6a82ecf1db11bc831931b69b635e276
Samantha Fernandes <samsam.sammy.f@gmail.com>
2026-07-04 15:34:20 +0900

    need to ask about wtf is happening with the update

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,16 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   get "tasks", to: "tasks#index", as: :tasks
+  # new
+  get "tasks/new", to: "tasks#new", as: :new_task
   # show
   get "/tasks/:id", to: "tasks#show", as: :task
+  # create
+  post "tasks", to: "tasks#create"
+
+  # edit
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  # update
+  patch "tasks/:id", to: "tasks#update"
+  delete "tasks/:id", to: "tasks#destroy"
 end
